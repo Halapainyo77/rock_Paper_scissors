52a6ade2aa5c4ae5498b199aca289e5ad3721ba7
Halapainyo77 <137466756+Halapainyo77@users.noreply.github.com>
2024-01-26 14:39:44 -0700

    Create function for a random selection by the computer

---
+++ script.js
@@ -0,0 +1,3 @@
+const getcomputerChoice =["Rock","Paper","Scissors"]
+        const random = Math.floor(Math.random() * getcomputerChoice.length);
+         console.log(random,getcomputerChoice[random]);
