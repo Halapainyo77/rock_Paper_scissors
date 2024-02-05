2cdb374ee19bc28caae0220f0f7536cb71755937
Halapainyo77 <137466756+Halapainyo77@users.noreply.github.com>
2024-02-05 16:20:52 -0700

    Create way for player to select from options while being case insensitive

--- script.js
+++ script.js
@@ -4,4 +4,5 @@ let computerScore = 0;
 const gameChoices =["Rock","Paper","Scissors"]
 const getComputerChoice = gameChoices[Math.floor(Math.random() * gameChoices.length)];
 
-        
+const playerSelection = prompt ("Rock, paper, or scissors, what do you choose human?").toLowerCase();
+      
