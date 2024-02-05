92ed6b75f9f02c646e8e387d34a804440aac3d53
Halapainyo77 <137466756+Halapainyo77@users.noreply.github.com>
2024-02-05 16:18:44 -0700

    Create baseline score for both computer and human players

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+let playerScore = 0;
+let computerScore = 0;
+
 const gameChoices =["Rock","Paper","Scissors"]
 const getComputerChoice = gameChoices[Math.floor(Math.random() * gameChoices.length)];
 
